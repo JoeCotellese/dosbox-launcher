ef3885686ca1b57a8414471de42e926b2cdcfc90
Joe Cotellese <joe@cotellese.net>
2024-03-15 09:11:03 -0400

    first working version

--- dosbox_launcher/launcher.py
+++ dosbox_launcher/launcher.py
@@ -29,16 +29,23 @@ def main():
         print("missing config file, will assume defaults")
         dosbox_conf = None
 
-    # load the sub-folder C.harddrive
-    gamedrive = os.path.join(folder, "C.harddrive")
-    if not os.path.exists(gamedrive):
-        print("missing game folder")
+    # get a list of all of the subfolders in the folder
+    subfolders = [f for f in os.listdir(folder) 
+                  if os.path.isdir(os.path.join(folder, f))]
+    if len(subfolders) != 1:
+        print("missing game folder, or I can't tell which one to use.  I found these: ", subfolders)
         sys.exit(1)
+    gamedrive = subfolders[0]   
+    # if not os.path.exists(gamedrive):
+    #     print(f"missing game folder {gamedrive}")
+    #     sys.exit(1)
 
     # Launch dosbox
     DOSBOX_LOCATION = "/Applications/dosbox-x.app/Contents/MacOS/dosbox-x"
+    # change the working directory to the folder
+    os.chdir(folder)
     subprocess.run([DOSBOX_LOCATION, "-conf", dosbox_conf, "-c",
-                    "mount c " + gamedrive, "-c", "c:", "-c", "exit"])
+                    "mount c " + gamedrive, "-c", "c:"])
 
 
 if __name__ == "__main__":
